9ad5fb27a798fcd0490cebbfdd15d9883b54b626
Xintong-ZHAI <143562159+Xintong-ZHAI@users.noreply.github.com>
2024-08-15 07:58:26 -0400

    Add files via upload

---
+++ R/Step2_Coarse Classification.r
@@ -0,0 +1,164 @@
+library(Seurat)
+
+sub_celltype_insitutypeML <- function(cluster, sub_celltype){
+  #########subclustering for mixed main celltypes
+  c = cluster
+  temp <- subsetGiotto(xenium_gobj_updated, cell_ids=xenium_gobj_updated@cell_ID$cell[pDataDT(xenium_gobj_updated)$cell_type_majorvote==c])
+  
+  temp = runPCA(gobject = temp,spat_unit = 'cell',expression_values = 'normalized',feats_to_use = NULL,scale_unit = F,center = F)
+  
+  # run Harmony
+  temp = runGiottoHarmony(temp, vars_use = "fov",dim_reduction_to_use = "pca",dimensions_to_use = 1:25)
+  
+  # Generate UMAP from Harmony
+  temp = runUMAP(temp,dim_reduction_to_use = "harmony",dim_reduction_name = "harmony",dimensions_to_use = 1:25,n_threads = 4)
+  
+  
+  # sNN and Leiden clustering
+  temp = createNearestNetwork(temp,
+                              type = "sNN",
+                              dim_reduction_to_use = "umap",
+                              dim_reduction_name = "umap",
+                              dimensions_to_use = 1:2, name="sNN.umap")
+  
+  # the resolution here defaultly to be 0.1, but can be customized by the user
+  # the higher the resolution is, the more clusters there will be
+  temp = doLeidenCluster(temp,
+                         network_name = "sNN.umap",
+                         resolution = 0.05,
+                         n_iterations = 1000)
+  
+  
+  ### in situ type for T-cell subclustering
+  temp.cell_info = pDataDT(xenium_gobj_updated)
+  prb_ind <- rownames(xenium_gobj_updated@expression$cell$neg_probe$raw@exprMat)
+  
+  # input 1: A spatial matrix of counts data, cells x genes
+  # input 2: A vector giving each cell’s mean negative control value
+  temp.exp_mat <- list()
+  temp.negmean <- list()
+  
+  ### giotto normalized matrix
+  temp.exp_mat[["gio_norm"]] <- as.matrix(temp@expression$cell$rna$normalized@exprMat) + 1e-7 ### gene x cell matrix  
+  temp.negmean[["gio_norm"]]  <- colMeans(as.matrix(temp@expression$cell$neg_probe$normalized@exprMat))### 
+  #temp.negmean[["gio_norm"]]  <- weighted_sum
+  
+  ### giotto normalized matrix and batch corrected
+  a <- adjustGiottoMatrix(temp, expression_values="normalized", feat_type="rna", batch_columns="list_ID")
+  a <- a@expression$cell$rna$custom@exprMat
+  temp.exp_mat[["gio_norm_batch"]] <- a
+  
+  a <- adjustGiottoMatrix(temp, expression_values="normalized", feat_type="neg_probe", batch_columns="list_ID")
+  a <- a@expression$cell$neg_probe$custom@exprMat
+  temp.negmean[["gio_norm_batch"]] <- Matrix::colMeans(a)
+  
+  ### remove cells with zero counts for all genes
+  for (i in names(temp.exp_mat)){
+    temp.exp_mat[[i]] <- temp.exp_mat[[i]][,colSums(temp.exp_mat[[i]])!=0]
+    mode(temp.exp_mat[[i]]) <- "integer"
+  }
+  
+  for (i in names(temp.exp_mat)){
+    temp.negmean[[i]] <- temp.negmean[[i]][colnames(temp.exp_mat[[i]])]
+  }
+  
+  # input 3: A "reference matrix" giving the expected expression profile of each cell type,
+  # with genes in rows and cell types in columns. The reference matrix must be in linear-scale, not log-scale.
+  temp.ref_mat <- list()
+  temp.sc <- as.matrix(read.table("/home/zhaixt/global_ct_DEgenes_exp.csv",row.names=1,header=T,sep=",",quote=NULL,comment.char="",check.names=F))
+  row.names(temp.sc)<- gsub("\"","",row.names(temp.sc) )
+  colnames(temp.sc)<- gsub("\"","",colnames(temp.sc) )
+  
+  #### !!!! need to select the sub-celltypes
+  positions <- match(sub_celltype, colnames(temp.sc))
+  if (length(positions) == 0){
+    stop("sub_celltype not found in the reference matrix")
+  }
+  temp.ref_mat[["ref.scRNA"]] <- temp.sc[, positions]
+  
+  
+  # input 4: 
+  ## Incorporating additional data types: use immunofluorescence and spatial coordinates data for cohorting
+  ## immunofluorescence
+  
+  spatial_location <- get_spatial_locations(temp,spat_unit = NULL,spat_loc_name = NULL,output = c("spatLocsObj", "data.table"),copy_obj = TRUE,verbose = TRUE,set_defaults = TRUE) 
+  
+  xy_coordinates <- spatial_location@coordinates 
+  
+  temp.umap <- temp@dimension_reduction$cells$cell$rna$umap$umap@coordinates
+  temp.meta_info <- cbind(xy_coordinates[,-1], temp.umap)
+  rownames(temp.meta_info) <- xy_coordinates$cell_ID
+  
+  temp.cohort <- list()
+  temp.cohort.value <- fastCohorting(temp.meta_info, gaussian_transform = T) 
+  
+  ### if directly using the expression cluster
+  ### temp.cohort.value <- pDataDT(temp)$leiden_clus
+  
+  ##########################################################
+  ### do the in situtype supervised clustering one fov at a time
+  
+  fs <- as.data.frame(unique(temp.cell_info[,3]))[,1]
+  temp.is.ML <- list()
+  
+  for (i in names(temp.exp_mat)[1]){
+    print(i)
+    temp.is.ML[[i]] <- list()
+    for (f in fs){
+      print(f)
+      templogical <- grep(colnames(temp.exp_mat[[i]]),pattern=f)
+      
+      temp.is.ML[[i]][[f]] <- insitutypeML (x=t(temp.exp_mat[[i]][,templogical]), neg=temp.negmean[[i]][templogical], cohort=temp.cohort.value[templogical],reference_profiles=temp.ref_mat[["ref.scRNA"]])
+    }
+  }
+  
+  ########################
+  # project to original space
+  
+  tempcell = xenium_gobj_updated@cell_metadata$cell$rna$cell_type_isML_updated
+  names(tempcell) = xenium_gobj_updated@cell_ID$cell
+  
+  i=1
+  for (f in names(temp.is.ML[[i]])){
+    a <- temp.is.ML[[i]][[f]]$clust
+    tempcell[names(a)] <- a
+  }
+  
+  xenium_gobj_updated = annotateGiotto(gobject = xenium_gobj_updated,
+                                       annotation_vector = tempcell,
+                                       cluster_column = 'cell_ID',
+                                       name = 'cell_type_isML_updated')
+  
+  return(xenium_gobj_updated)
+  
+}
+
+xenium_gobj_updated = xenium_gobj
+
+main_celltype_list = unique(xenium_gobj_updated@cell_metadata$cell$rna$cell_type_majorvote) #15
+
+xenium_gobj_updated@cell_metadata$cell$rna$cell_type_isML_updated = xenium_gobj_updated@cell_metadata$cell$rna$cell_type_majorvote
+
+for (main_celltype in main_celltype_list[5]) {
+  contains_comma = grepl(",", main_celltype)
+  if (contains_comma){
+    sub_celltype_list = as.vector(strsplit(main_celltype, ", ")[[1]])
+    print(sub_celltype_list)
+    xenium_gobj_updated = sub_celltype_insitutypeML(main_celltype, sub_celltype_list)
+  }
+}
+
+saveGiotto(xenium_gobj_updated, save_dir = results_folder, save_name = "gobj_updated")
+
+
+plotUMAP(gobject = xenium_gobj_updated,
+         cell_color = 'cell_type_isML_updated',
+         cell_color_code = col_vector,
+         show_NN_network = F, 
+         show_center_label=F,
+         nn_network_to_use='sNN', 
+         network_name="sNN.umap",
+         point_size = 1,
+         dim_reduction_name="umap",
+         save_param = list(save_name = '8.1_UMAP_cell.types.updated_isML',base_width=15,base_height=12)
+)
---
+++ R/Step3_Precise Classification.r
@@ -0,0 +1,217 @@
+library(BASS)
+
+#xenium_gobj=loadGiotto("/home/alextsoi/Researches/Xenium_skin_UMcustom.panel/analysis/gobj")
+#obj_ids = paste0("obj",1:length(xenium_folders))
+# psoriasis
+xenium_gobj = loadGiotto("/home/zhaixt/Xenium_Janssen_0715/gobj_updated_0719")
+
+# set working directory
+#results_folder = '/path/to/save/directory/'
+#results_folder = getwd()
+results_folder = "/home/zhaixt/Xenium_Janssen_0715/"
+setwd(results_folder)
+
+instrs = createGiottoInstructions(save_dir = results_folder,
+                                  save_plot = TRUE,
+                                  show_plot = FALSE,
+                                  return_plot = FALSE)
+
+xenium_gobj = replaceGiottoInstructions(xenium_gobj, instructions = instrs)
+
+c1="Myeloid.Cells"
+# c2="T.Cells"
+# temp <- subsetGiotto(xenium_gobj, cell_ids=xenium_gobj@cell_ID$cell[(pDataDT(xenium_gobj)$cell_type_isML_updated==c1) | (pDataDT(xenium_gobj)$cell_type_isML_updated==c2)])
+temp <- subsetGiotto(xenium_gobj, cell_ids=xenium_gobj@cell_ID$cell[(pDataDT(xenium_gobj)$cell_type_isML_updated==c1)])
+
+
+# get cell info
+cell_info = pDataDT(temp)[,c(1,3,6,7,10,11)]
+cell_info = as.data.frame(cell_info)
+head(cell_info)
+
+# position
+spatial_location = get_spatial_locations(temp,spat_unit = NULL,spat_loc_name = NULL,output = c("spatLocsObj", "data.table"),copy_obj = TRUE,verbose = TRUE,set_defaults = TRUE) 
+pos_file <- spatial_location@coordinates 
+pos_file = as.data.frame(pos_file)
+rownames(pos_file) <- cell_info$cell_ID
+pos_file <- pos_file[,c(2,3)]
+colnames(pos_file) <- c("x","y")
+
+# raw count matrix
+raw_cnts = temp@expression$cell$rna$raw@exprMat + 1e-8
+raw_cnts = as.matrix(raw_cnts)
+
+sum(rownames(pos_file) != colnames(raw_cnts))
+
+# create lists of counts and xy coordinates
+fov_names = unique(cell_info$list_ID)
+cnts = list()
+xy = list()
+clust = list()
+clust_ref = 1:length(unique(cell_info$leiden_clus)) - 1
+names(clust_ref) = unique(cell_info$leiden_clus)
+for (i in 1:length(fov_names)){
+  cell_ids = cell_info[cell_info$list_ID == fov_names[i], "cell_ID"]
+  cnts[[i]] = as.matrix(raw_cnts[,cell_ids])
+  xy[[i]] = pos_file[cell_ids,]
+  clust[[i]] = cell_info[cell_info$list_ID == fov_names[i], "leiden_clus"] - 1
+  clust[[i]] = clust_ref[cell_info[cell_info$list_ID == fov_names[i], "leiden_clus"]]
+}
+
+names(cnts) = fov_names
+names(xy) = fov_names
+names(clust) = fov_names
+
+# create a BASS object
+BASS <- createBASSObject(cnts, xy, C = 7, R = 5, beta_method = "SW")
+listAllHyper(BASS)
+BASS <- BASS.preprocess(BASS)
+# run BASS algorithm using self-provided clustering labels
+#BASS <- BASS.run(BASS, clust)
+BASS <- BASS.run(BASS)
+# Post-process posterior samples
+BASS <- BASS.postprocess(BASS)
+clabels <- BASS@results$c # cell type clusters
+zlabels <- BASS@results$z # spatial domain labels
+pi_est <- BASS@results$pi # cell type composition matrix
+
+giotto_c = c()
+giotto_z = c()
+for (i in 1:length(clabels)){
+  giotto_c = c(giotto_c,clabels[[i]])
+  giotto_z = c(giotto_z,zlabels[[i]])
+}
+names(giotto_c) = pDataDT(temp)$cell_ID
+names(giotto_z) = pDataDT(temp)$cell_ID
+
+temp = annotateGiotto(gobject = temp,
+                          annotation_vector = giotto_c,
+                          cluster_column = 'cell_ID',
+                          name = 'bass_clus_M')
+
+temp = annotateGiotto(gobject = temp,
+                          annotation_vector = giotto_z,
+                          cluster_column = 'cell_ID',
+                          name = 'bass_domain_M')
+
+plotUMAP(temp,
+         cell_color = 'bass_clus_M',
+         cell_color_code = col_vector,
+         point_size = 1,
+         save_param = list(save_name = 'UMAP_bass_clust_M'))
+
+plotUMAP(temp,
+         cell_color = 'bass_domain_M',
+         cell_color_code = col_vector,
+         point_size = 1,
+         save_param = list(save_name = 'UMAP_bass_domain_M'))
+
+spatInSituPlotPoints(xenium_gobj,
+                     show_polygon = TRUE,
+                     polygon_color = 'white',
+                     polygon_line_size = 0.01,
+                     polygon_fill = 'bass_clus',
+                     polygon_fill_as_factor = TRUE,
+                     polygon_fill_code = col_vector,
+                     polygon_alpha = 1, # transparency of polygon colors
+                     save_param = list(base_height = 10, dpi = 1000,
+                                       save_name = 'inSitu_bass_cluster'))
+spatInSituPlotPoints(xenium_gobj,
+                     show_polygon = TRUE,
+                     polygon_color = 'white',
+                     polygon_line_size = 0.01,
+                     polygon_fill = 'bass_domain',
+                     polygon_fill_as_factor = TRUE,
+                     polygon_fill_code = col_vector,
+                     polygon_alpha = 1, # transparency of polygon colors
+                     save_param = list(base_height = 10, dpi = 1000,
+                                       save_name = 'inSitu_bass_domain'))
+
+
+########## sub-celltyping
+
+### add the in situ cluster label to giotto
+temp.cell_info = pDataDT(xenium_gobj)
+prb_ind <- rownames(xenium_gobj@expression$cell$neg_probe$raw@exprMat)
+
+tempcell = xenium_gobj@cell_metadata$cell$rna$cell_type_isML_updated.sub
+names(tempcell) = xenium_gobj@cell_ID$cell
+
+temp.exp_mat <- list()
+temp.negmean <- list()
+
+### giotto normalized matrix
+temp.exp_mat[["gio_norm"]] <- as.matrix(temp@expression$cell$rna$raw@exprMat) ### gene x cell matrix  
+temp.negmean[["gio_norm"]]  <- colMeans(as.matrix(temp@expression$cell$neg_probe$raw@exprMat))### 
+#temp.negmean[["gio_norm"]]  <- weighted_sum
+
+### giotto normalized matrix and batch corrected
+a <- adjustGiottoMatrix(temp, expression_values="normalized", feat_type="rna", batch_columns="list_ID")
+a <- a@expression$cell$rna$custom@exprMat
+temp.exp_mat[["gio_norm_batch"]] <- a
+
+a <- adjustGiottoMatrix(temp, expression_values="normalized", feat_type="neg_probe", batch_columns="list_ID")
+a <- a@expression$cell$neg_probe$custom@exprMat
+temp.negmean[["gio_norm_batch"]] <- Matrix::colMeans(a)
+
+### remove cells with zero counts for all genes
+for (i in names(temp.exp_mat)){
+  temp.exp_mat[[i]] <- temp.exp_mat[[i]][,colSums(temp.exp_mat[[i]])!=0]
+  mode(temp.exp_mat[[i]]) <- "integer"
+}
+
+for (i in names(temp.exp_mat)){
+  temp.negmean[[i]] <- temp.negmean[[i]][colnames(temp.exp_mat[[i]])]
+}
+# input 3: A "reference matrix" giving the expected expression profile of each cell type,
+# with genes in rows and cell types in columns. The reference matrix must be in linear-scale, not log-scale.
+temp.ref_mat <- list()
+temp.sc <- as.matrix(read.table("/home/zhaixt/Myeloid Cells_subset.csv",row.names=1,header=T,sep=",",quote=NULL,comment.char="",check.names=F))
+row.names(temp.sc) <- gsub("\"","",row.names(temp.sc) )
+colnames(temp.sc) <- gsub("\"","",colnames(temp.sc) )
+  
+temp.ref_mat[["ref.scRNA"]] <- temp.sc
+temp.cohort.value <- giotto_c
+
+fs <- as.data.frame(unique(temp.cell_info[,3]))[,1]
+temp.is.ML <- list()
+
+for (i in names(temp.exp_mat)[1]){
+  print(i)
+  temp.is.ML[[i]] <- list()
+  for (f in fs){
+    print(f)
+    templogical <- grep(colnames(temp.exp_mat[[i]]),pattern=f)
+    temp.is.ML[[i]][[f]] <- insitutypeML(x=t(temp.exp_mat[[i]][,templogical]), neg=temp.negmean[[i]][templogical], cohort=temp.cohort.value[templogical],reference_profiles=temp.ref_mat[["ref.scRNA"]])
+  }
+}
+
+
+
+### looks like the giotto normalized matrix provides reasonable results
+i=1
+for (f in names(temp.is.ML[[i]])){
+  a <- temp.is.ML[[i]][[f]]$clust
+  tempcell[names(a)] <- a
+}
+
+#xenium_gobj@cell_metadata$cell$rna$sub_cell_type_isML_updated = xenium_gobj@cell_metadata$cell$rna$cell_type_isML_updated.sub
+
+xenium_gobj = annotateGiotto(gobject = xenium_gobj,
+                                       annotation_vector = tempcell,
+                                       cluster_column = 'cell_ID',
+                                       name = 'sub_cell_type_isML_updated')
+
+plotUMAP(gobject = xenium_gobj,
+         cell_color = 'sub_cell_type_isML_updated',
+         cell_color_code = col_vector,
+         show_NN_network = F, 
+         show_center_label=F,
+         nn_network_to_use='sNN', 
+         network_name="sNN.umap",
+         point_size = 1,
+         dim_reduction_name="umap",
+         save_param = list(save_name = '8.1_UMAP_M_T.sub.cell.types.updated_isML',base_width=15,base_height=12)
+)
+
+saveGiotto(xenium_gobj, save_dir = results_folder, save_name = "gobj_updated_0723")
