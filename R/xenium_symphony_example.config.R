f143cd1488fb6fc4cd08280f05aef554a662fa71
yulicai <yulicai@umich.edu>
2024-09-11 21:25:52 -0400

    improved symphony pipeline by Alex

---
+++ R/xenium_symphony_example.config.R
@@ -0,0 +1,39 @@
+########## Set Parameters ##########
+set.seed(0)
+
+## Build reference
+skip_build_ref_main=FALSE
+skip_build_ref_sub = FALSE # nolint
+ref_exp_path = '/home/yulicai/symphony/exprs_norm.rds'	# janssen ad
+ref_metadata_path = '/home/yulicai/symphony/metadata.rds'
+ref_exp = readRDS(ref_exp_path)	
+ref_metadata = readRDS(ref_metadata_path)
+maintype_col_name = 'celltype.global'  # column in metadata representing main celltype
+subtype_col_name = 'celltype.sub'   # column in metadata representing sub celltype
+
+### if provide a seurat object directly
+#seurat_obj = readRDS('/home/alextsoi/db/psoriasis.scRNA/seurat_with_rachael_subtype.RDS')
+#ref_exp = seurat_obj$RNA@data    # gene x cell, should be log(CP10K + 1) normalized 
+#ref_metadata = seurat_obj@meta.data
+#maintype_col_name = 'celltype.global'  # column in metadata representing main celltype
+#subtype_col_name = 'celltype.sub'   # column in metadata representing sub celltype
+
+downsample = TRUE    # You may need downsampling when you have too many cells.
+downsample_to = 10000  # The number of cells you want to keep. Final number may be slightly different. Ignore it when downsample=FALSE.
+vars_use = c('orig.ident') # Harmony parameter. If meta_data is dataframe, this defined which variable(s) to remove (character vector).
+
+save_main_ref_dir = '/home/alextsoi/test/symphony'
+save_main_uwot_dir = '/home/alextsoi/test/symphony'
+save_sub_ref_dir = '/home/alextsoi/test/symphony'
+save_sub_uwot_dir = '/home/alextsoi/test/symphony'
+
+
+## Query
+output_dir <- '/home/alextsoi/test/symphony'
+
+### provide full paths of all cell_features_matrix.h5 file of the xenium data
+query_paths <- as.matrix(read.table("xxxxx"))[,1]
+###query_paths <- c('/hits/AGC/Spatial/Xenium/hSkin_481g/10265-JF_and_10628-JF/20240517__214754__10265_10628-JF/20240517__214754__10265_10628-JF/output-XETG00077__0022387__10628-JF-1_ROI_A1__20240517__215149/cell_feature_matrix.h5',              '/hits/AGC/Spatial/Xenium/hSkin_480g/10265-JF_and_10628-JF/20240517__214754__10265_10628-JF/20240517__214754__10265_10628-JF/output-XETG00077__0022387__10628-JF-1_ROI_A2__20240517__215149/cell_feature_matrix.h5','/hits/AGC/Spatial/Xenium/hSkin_480g/10265-JF_and_10628-JF/20240517__214754__10265_10628-JF/20240517__214754__10265_10628-JF/output-XETG00077__0022387__10628-JF-1_ROI_B__20240517__215149/cell_feature_matrix.h5','/hits/AGC/Spatial/Xenium/hSkin_480g/10265-JF_and_10628-JF/20240517__214754__10265_10628-JF/20240517__214754__10265_10628-JF/output-XETG00077__0022387__10628-JF-1_ROI_C1__20240517__215149/cell_feature_matrix.h5')
+
+k = 5   # the k of KNN in mapping.
+
